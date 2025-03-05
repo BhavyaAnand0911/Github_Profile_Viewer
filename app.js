943a741773d7ce9048dc574c822acc3dc002da48
BhavyaAnand0911 <bhavyaanand0911@gmail.com>
2025-03-06 01:35:26 +0530

    made a single page application and resolved some minor bugs

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-console.log("Working");
+var app = angular.module("GitHubProfileViewer", []);
+app.controller("MainController", function ($scope, $http) {
+  $scope.users = [];
+  $scope.selectedUser = null;
+  $scope.repos = [];
+
+  // Search for users
+  $scope.search = function () {
+    $http
+      .get("https://api.github.com/search/users?q=" + $scope.searchUser)
+      .then(function (response) {
+        $scope.users = response.data.items;
+
+        $scope.users.forEach((user) => {
+          $http
+            .get("https://api.github.com/users/" + user.login)
+            .then(function (detailsResponse) {
+              Object.assign(user, detailsResponse.data);
+            });
+        });
+      });
+  };
+
+  $scope.viewProfile = function (username) {
+    $http
+      .get("https://api.github.com/users/" + username)
+      .then(function (response) {
+        $scope.selectedUser = response.data;
+        return $http.get(response.data.repos_url);
+      })
+      .then(function (response) {
+        $scope.repos = response.data;
+      });
+  };
+
+  $scope.clearProfile = function () {
+    $scope.selectedUser = null;
+    $scope.repos = [];
+  };
+});
